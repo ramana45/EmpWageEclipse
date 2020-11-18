22dde11acc170c51cae877c012ed02fd334a628e
ramana <ramanarajupyla@gmail.com>
2020-11-18 16:34:22 +0530

    Adding EmpWage_UC3 file

--- src/com/example/EmpWage.java
+++ src/com/example/EmpWage.java
@@ -4,15 +4,20 @@ public class EmpWage {
 	public static void main(String[] args) {
 		System.out.println("***Employee Wage Builder Problem***");
 		// Constants
-		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 1;
+		int IS_FULL_TIME = 2;
 		int EMP_RATE_PER_HR = 20;
 		// Variables
 		int empHrs = 0;
 		int empWage = 0;
 		// Computation
-		double empcheck = Math.floor(Math.random() * 10) % 2;
-		if(empcheck == IS_FULL_TIME) {
-			System.out.println("Employee is Present");
+		double empcheck = Math.floor(Math.random() * 10) % 3;
+		if(empcheck == IS_PART_TIME) {
+			System.out.println("Employee is Present and Part time");
+			empHrs = 4;
+		}
+		else if(empcheck == IS_FULL_TIME){
+			System.out.println("Employee is Present and Full time");
 			empHrs = 8;
 		}
 		else {
