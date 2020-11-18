81b2febc9f26af9a94be439bdeffcf0b3bb59e07
ramana <ramanarajupyla@gmail.com>
2020-11-18 16:13:59 +0530

    Adding EmpWage_UC1 file

--- src/com/example/EmpWage.java
+++ src/com/example/EmpWage.java
@@ -2,7 +2,14 @@ package com.example;
 
 public class EmpWage {
 	public static void main(String[] args) {
-		System.out.println("Employee Wage Builder Problem");
-		
+		System.out.println("***Employee Wage Builder Problem***");
+		// Constants
+		int IS_FULL_TIME = 1;
+		// Computation
+		double empcheck = Math.floor(Math.random() * 10) % 2;
+		if(empcheck == IS_FULL_TIME)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Abscent");
 	}
 }
