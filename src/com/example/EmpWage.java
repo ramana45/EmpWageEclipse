9dc6b4aec68e202e582102010eb0c8dcfa270e14
ramana <ramanarajupyla@gmail.com>
2020-11-18 17:36:22 +0530

    Adding EmpWage_UC4 file

--- src/com/example/EmpWage.java
+++ src/com/example/EmpWage.java
@@ -1,29 +1,31 @@
 package com.example;
 
-public class EmpWage {
-	public static void main(String[] args) {
-		System.out.println("***Employee Wage Builder Problem***");
-		// Constants
-		int IS_PART_TIME = 1;
-		int IS_FULL_TIME = 2;
-		int EMP_RATE_PER_HR = 20;
-		// Variables
-		int empHrs = 0;
-		int empWage = 0;
-		// Computation
-		double empcheck = Math.floor(Math.random() * 10) % 3;
-		if(empcheck == IS_PART_TIME) {
-			System.out.println("Employee is Present and Part time");
-			empHrs = 4;
-		}
-		else if(empcheck == IS_FULL_TIME){
-			System.out.println("Employee is Present and Full time");
-			empHrs = 8;
-		}
-		else {
-			System.out.println("Employee is Abscent");
-			empHrs = 0;
-		}
+	public class EmpWage {
+
+		public static final int IS_PART_TIME = 1;
+		public static final int IS_FULL_TIME = 2;
+		public static final int EMP_RATE_PER_HR = 20;
+	
+		public static void main(String args[]) {
+			System.out.println("***Employee Wage Builder Problem***");
+			// Variables
+			int empHrs = 0;
+			int empWage = 0;
+			// Computation
+			int empcheck = (int) Math.floor(Math.random() * 10) % 3;
+			switch (empcheck) {
+				case IS_PART_TIME:
+					empHrs = 4;
+					System.out.println("Employee is Present and Part time");
+					break;
+				case IS_FULL_TIME:
+					empHrs = 8;
+					System.out.println("Employee is Present and Full time");		
+					break;
+				default:
+					empHrs = 0;
+					System.out.println("Employee is Abscent");
+			}
 		empWage = empHrs * EMP_RATE_PER_HR;
 		System.out.println("Emp Wage: "+empWage);
 	}
